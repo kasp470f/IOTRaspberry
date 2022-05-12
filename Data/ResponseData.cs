d76f2edcf1f36b41be815063aad134cf18ea89c2
Max <madstk1@pm.me>
2022-05-12 10:34:59 +0200

    Initial commit

---
+++ Data/ResponseData.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace webapp.Data {
+    public class ResponseData {
+        public float PumpFlow { get; set; }
+        public float PowerUsage { get; set; }
+        public float Frequency { get; set; }
+    }
+}
